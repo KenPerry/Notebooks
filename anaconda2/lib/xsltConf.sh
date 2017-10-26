7c68a05accc6000275f7869fecd63be72982ba66
K Perry <ken.perry.quant@gmail.com>
2017-10-26 20:24:11 +0000

    added Modern Pandas Tidy Data

--- anaconda2/lib/xsltConf.sh
+++
@@ -1,7 +0,0 @@
-#
-# Configuration file for using the xslt library
-#
-XSLT_LIBDIR="-L/home/ubuntu/anaconda2/lib"
-XSLT_LIBS="-lxslt  -L/home/ubuntu/anaconda2/lib -lxml2 -L/home/ubuntu/anaconda2/lib -lz -L/home/ubuntu/anaconda2/lib -liconv -lm -ldl -lm -lrt"
-XSLT_INCLUDEDIR="-I/home/ubuntu/anaconda2/include"
-MODULE_VERSION="xslt-1.1.29"
--- anaconda2/pkgs/pandas-0.18.1-np111py27_0/info/recipe/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ `uname` == Darwin ]; then
-    LDFLAGS="${LDFLAGS} -headerpad_max_install_names"
-fi
-
-$PYTHON setup.py install --old-and-unmanageable
